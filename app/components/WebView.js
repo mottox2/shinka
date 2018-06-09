b183ff21ccfd787f103287b3d69375460dfac12b
mottox2 <y.mottox2@gmail.com>
2018-06-09 19:01:50 +0900

    Get video_id from webview

--- app/components/WebView.js
+++ app/components/WebView.js
@@ -1,21 +1,37 @@
 import React from 'react';
 
 class WebView extends React.Component {
+  state = {
+    currentVideoId: null
+  };
+
   componentDidMount() {
     this.webview.addEventListener('will-navigate', event => {
       console.log(event.url);
+    });
+    this.webview.addEventListener('page-title-updated', () => {
+      const url = new URL(this.webview.getURL());
+      const currentVideoId = url.searchParams.get('v');
+      if (currentVideoId && this.webview.canGoBack()) {
+        this.setState({ currentVideoId });
+        this.webview.goBack();
+      }
     });
   }
 
   render() {
     return (
-      <webview
-        ref={webview => {
-          if (webview) this.webview = webview;
-        }}
-        src="http://qiita.com"
-        autosize="on"
-      />
+      <React.Fragment>
+        {this.state.currentVideoId}
+        <webview
+          ref={webview => {
+            if (webview) this.webview = webview;
+          }}
+          style={{ minHeight: 600 }}
+          src="http://youtube.com"
+          autosize="on"
+        />
+      </React.Fragment>
     );
   }
 }
